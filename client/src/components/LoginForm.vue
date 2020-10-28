b0128e8eb1c7aa478a524ffa46979bbdede3d4eb
IktaS <imamrafiia@gmail.com>
2020-10-28 17:36:26 +0700

    [Client][+]: Added Sign Up Button to Login Form

--- client/src/components/LoginForm.vue
+++ client/src/components/LoginForm.vue
@@ -1,29 +1,36 @@
 <template>
   <v-card>
-    <v-form ref="form" v-model="valid">
-      <v-container>
+    <v-container>
+      <v-form ref="form" v-model="valid">
         <v-row justify="center">
-          <v-col cols="12">
+          <v-col cols="11">
             <h1>Login to Portofolion</h1>
           </v-col>
         </v-row>
         <v-row justify="center">
-          <v-col cols="12">
+          <v-col cols="11">
             <v-text-field label="Username or Email" outlined> </v-text-field>
           </v-col>
         </v-row>
         <v-row justify="center">
-          <v-col cols="12">
+          <v-col cols="11">
             <v-text-field label="Password" outlined> </v-text-field>
           </v-col>
         </v-row>
         <v-row justify="center">
-          <v-col :cols="$vuetify.breakpoint.mobile ? '12' : '4'">
+          <v-col :cols="$vuetify.breakpoint.mobile ? '10' : '4'">
             <v-btn label="Login" block outlined>Login</v-btn>
           </v-col>
         </v-row>
-      </v-container>
-    </v-form>
+      </v-form>
+      <v-row justify="center">
+        <v-col :cols="$vuetify.breakpoint.mobile ? '10' : '4'">
+          <v-btn label="Sign Up" text x-small block to="/signup">
+            Sign Up
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-card>
 </template>
 
