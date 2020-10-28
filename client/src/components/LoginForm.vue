b72bb57065ded740ad131754a7f8c252da9f1ed2
IktaS <imamrafiia@gmail.com>
2020-10-28 17:16:33 +0700

    [Client][+]: Made login form UI

--- client/src/components/LoginForm.vue
+++ client/src/components/LoginForm.vue
@@ -1,6 +1,29 @@
 <template>
   <v-card>
-    <h1>Form Here</h1>
+    <v-form ref="form" v-model="valid">
+      <v-container>
+        <v-row justify="center">
+          <v-col cols="12">
+            <h1>Login to Portofolion</h1>
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-col cols="12">
+            <v-text-field label="Username or Email" outlined> </v-text-field>
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-col cols="12">
+            <v-text-field label="Password" outlined> </v-text-field>
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-col :cols="$vuetify.breakpoint.mobile ? '12' : '4'">
+            <v-btn label="Login" block outlined>Login</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
   </v-card>
 </template>
 
