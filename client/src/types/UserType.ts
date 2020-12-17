5f656617ac255bf83d34ed3df9da171e10f8d911
IktaS <imamrafiia@gmail.com>
2020-12-17 20:14:44 +0700

    [Client][+]: fix

--- client/src/types/UserType.ts
+++ client/src/types/UserType.ts
@@ -1,25 +1,25 @@
 import Repo, { emptyRepo } from "./RepoType";
 
 export default interface User {
-	_id: string;
-	firstName: string;
-	lastName: string;
-	description: string;
-	username: string;
-	email: string;
-	profilePicture: unknown;
-	repos: Repo[];
+  _id: string;
+  firstName: string;
+  lastName: string;
+  description: string;
+  username: string;
+  email: string;
+  profilePicture: unknown;
+  repos: Repo[];
 }
 
 const emptyUser: User = {
-	_id: "",
-	firstName: "",
-	lastName: "",
-	description: "",
-	username: "",
-	email: "",
-	profilePicture: null,
-	repos: [emptyRepo],
+  _id: "",
+  firstName: "",
+  lastName: "",
+  description: "",
+  username: "",
+  email: "",
+  profilePicture: null,
+  repos: [emptyRepo]
 };
 
 export { emptyUser };
