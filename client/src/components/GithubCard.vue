57b466dbdf4e995839a3d1f652e8793c888cf81f
IktaS <imamrafiia@gmail.com>
2020-12-28 21:08:10 +0700

    [Client][+]: add languages to github card

--- client/src/components/GithubCard.vue
+++ client/src/components/GithubCard.vue
@@ -1,22 +1,46 @@
 <template>
   <v-card height="100%" outlined class="card-outter">
-    <v-card-title>{{ repo.name }}</v-card-title>
-    <v-card-text v-if="repo.description !== null">
-      {{ repo.description }}
-    </v-card-text>
-    <v-card-text v-if="repo.description === null">
-      No description
-    </v-card-text>
-    <v-card-actions class="card-actions">
-      <v-spacer />
-      <v-btn icon :href="repo.url">
-        <v-icon>mdi-github</v-icon>
-      </v-btn>
-      <v-btn icon @click="changeRepoVisibility" v-if="showVisibility">
-        <v-icon v-if="repo.isPublic">mdi-eye</v-icon>
-        <v-icon v-if="!repo.isPublic">mdi-eye-off</v-icon>
-      </v-btn>
-    </v-card-actions>
+    <v-container>
+      <v-row>
+        <v-card-title>{{ repo.name }}</v-card-title>
+      </v-row>
+      <v-row>
+        <v-card-text v-if="repo.description !== null">
+          {{ repo.description }}
+        </v-card-text>
+        <v-card-text v-if="repo.description === null">
+          No description
+        </v-card-text>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-chip-group column class="card-chips">
+            <v-chip
+              v-for="language in repo.languages"
+              :key="language"
+              disabled
+              small
+              text-color="white"
+              color="black"
+            >
+              {{ language }}
+            </v-chip>
+          </v-chip-group>
+        </v-col>
+        <v-col>
+          <v-card-actions class="card-actions">
+            <v-spacer />
+            <v-btn icon :href="repo.url">
+              <v-icon>mdi-github</v-icon>
+            </v-btn>
+            <v-btn icon @click="changeRepoVisibility" v-if="showVisibility">
+              <v-icon v-if="repo.isPublic">mdi-eye</v-icon>
+              <v-icon v-if="!repo.isPublic">mdi-eye-off</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-card>
 </template>
 
@@ -52,5 +76,11 @@ export default class GithubCard extends Vue {
   position: absolute;
   bottom: 0;
   right: 0;
+}
+
+.card-chips {
+  position: absolute;
+  bottom: 2rem;
+  left: 1rem;
 }
 </style>
